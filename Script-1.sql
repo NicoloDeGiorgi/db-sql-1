a7e7058a46bd6109d8f4edf49d22532e69cea169
nicolòdegiorgi <165511651+NicoloDeGiorgi@users.noreply.github.com>
2024-07-18 18:28:40 +0200

    Add files via upload

--- Script-1.sql
+++ Script-1.sql
@@ -6,6 +6,16 @@ where date_of_birth like "1990%" ;
 
 2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
 
+select *
+from courses 
+where cfu > "10";
+
+3.Selezionare tutti gli studenti che hanno più di 30 anni
+
+select *
+from students 
+where date_of_birth like  "1994%"
+order by date_of_birth ;
 
 
 
