e1fbc2c1b461c0ece6e5424bc7fdf10c248493f7
nicolòdegiorgi <165511651+NicoloDeGiorgi@users.noreply.github.com>
2024-07-18 18:14:41 +0200

    Add files via upload

---
+++ Script-1.sql
@@ -0,0 +1,11 @@
+1. Selezionare tutti gli studenti nati nel 1990 (160)
+
+select * 
+from students
+where date_of_birth like "1990%" ;
+
+2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
+
+
+
+
